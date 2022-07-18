3f4961ae54acbf61ac5d8733d25415ecf37ce463
Felipe Gomes <felipeagomes2202@gmail.com>
2022-07-18 18:07:31 +0100

    First Commit

---
+++ src/test/java/uk/co/webservices/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
